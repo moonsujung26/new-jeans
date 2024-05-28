d433080f274dbc1f83ed30d51dfa088922d01593
moonsujung26 <mmm021668@gmail.com>
2024-05-28 17:06:50 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function () {
+
+
+    $('.main_visual_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        fade: true,
+        pauseOnHover: false
+    });
+
+
+})
