033b508c7acd89f6272d1d3e961c632bbe236a30
moonsujung26 <mmm021668@gmail.com>
2024-05-29 01:01:12 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,27 @@
 $(function () {
-
-
     $('.main_visual_slide').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
         arrows: false,
         dots: true,
         fade: true,
         pauseOnHover: false
     });
 
+    function handleScroll() {
+        var scrollDowns = document.querySelector('.scroll-downs');
+        if (window.scrollY > 0) {
+            scrollDowns.style.display = 'none';
+        } else {
+            scrollDowns.style.display = 'block'; 
+        }
+    }
+    
+    window.addEventListener('scroll', handleScroll);
+
+    $('.link').on('change', function () {
+        const lnk = $(this).val();
+        console.log(lnk);
+        if (lnk) window.open(lnk)
+    });
 
 })
